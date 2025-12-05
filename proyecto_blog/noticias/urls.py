de40b41c8044905f2bf64e53160cdc9f6f033b4f
Rafael Strongoli <rafaelstrongoli@gmail.com>
2025-12-05 17:23:10 -0300

    agregados templates y detalles noticias

--- proyecto_blog/noticias/urls.py
+++ proyecto_blog/noticias/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import about, contacto  # from . import views
+from .views import inicio, about, contacto, lista_noticias  # from . import views
+from .views import NoticiaListView, DetalleNoticiaView
 
 urlpatterns = [
-    path("", about, name="about"),
-    path("contacto/", contacto, name="contacto")
+    path("", inicio, name="inicio"),
+    path("contacto/", contacto, name="contacto"),
+    # path("noticias/", lista_noticias, name="lista_noticias")
+    path("noticias/", NoticiaListView.as_view(), name="lista_noticias"),
+    path("noticias/<int:pk>", DetalleNoticiaView.as_view(), name="noticia_detalle")
+
 ]
--- proyecto_blog/noticias/views.py
+++ proyecto_blog/noticias/views.py
@@ -1,7 +1,19 @@
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Noticia
 
 # Create your views here.
+
+
+def inicio(request):
+    return render(request, "base.html")
+
+
+def lista_noticias(request):
+    contexto = Noticia.objects.all()
+    suma = 1 + 1
+    return render(request, "noticias/lista_noticias.html", {"noticias": contexto, "suma": suma})
 
 
 def about(request):
@@ -10,3 +22,18 @@ def about(request):
 
 def contacto(request):
     return render(request, "contacto.html")
+
+
+# vistas con Clases
+
+
+class NoticiaListView(ListView):
+    model = Noticia
+    template_name = "noticias/lista_noticias.html"
+    context_object_name = "noticias"
+
+
+class DetalleNoticiaView(DetailView):
+    model = Noticia
+    template_name = "noticias/detalle_noticia.html"
+    context_object_name = "noticia"
